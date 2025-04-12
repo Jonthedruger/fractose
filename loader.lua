cd1b994a8134a938b9436f46db0ece237fb1bca8
liebertmotion <137779989+Jonthedruger@users.noreply.github.com>
2025-04-12 13:16:28 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,3 @@
 if game.PlaceId == 76598287484083 then
-
+((loadstring or load)(game:HttpGet("https://raw.githubusercontent.com/Jonthedruger/fractose/refs/heads/main/games/animepower.lua", true)))()
 end
